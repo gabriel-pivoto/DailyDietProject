06596e42b5d058247491d29218cc45d1f7ffb75e
Gabriel Pivoto <gabrielspivoto@gmail.com>
2025-05-30 18:00:05 -0300

    feat: middleware

--- src/middlewares/check-session-id-exists.ts
+++ src/middlewares/check-session-id-exists.ts
@@ -1,8 +1,21 @@
 import { FastifyReply, FastifyRequest } from "fastify";
+import { knex } from "../database";
 
-export async function checkSessionIdExists(req: FastifyRequest, res: FastifyReply) {
-  const sessionId = req.cookies.sessionId;
+export async function checkSessionIdExists(
+  request: FastifyRequest,
+  reply: FastifyReply
+) {
+  const sessionId = request.cookies.sessionId;
+
   if (!sessionId) {
-    return res.status(401).send({ error: "Unauthorized" });
+    return reply.status(401).send({ error: "Unauthorized" });
   }
+
+  const user = await knex("users").where({ session_id: sessionId }).first();
+
+  if (!user) {
+    return reply.status(401).send({ error: "Unauthorized" });
+  }
+
+  request.user = user;
 }
