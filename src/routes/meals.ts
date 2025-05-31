6ca282e1dd1b6ffe9030bdc77f1241cf5b6dc028
Gabriel Pivoto <gabrielspivoto@gmail.com>
2025-05-30 23:46:05 -0300

    feat: update and  get meals

--- src/routes/meals.ts
+++ src/routes/meals.ts
@@ -25,4 +25,37 @@ export async function mealsRoutes(app: FastifyInstance) {
     });
     return res.status(201).send();
   });
+  app.put("/:id", { preHandler: [checkSessionIdExists] }, async (req, res) => {
+    const updateMealsBodySchema = z.object({
+      name: z.string(),
+      description: z.string(),
+      isOnDiet: z.boolean(),
+      date: z.coerce.date(),
+    });
+    const { name, description, isOnDiet, date } = updateMealsBodySchema.parse(
+      req.body
+    );
+    const { id } = req.params as { id: string };
+    const meal = await knex("meals")
+      .where({ id, user_id: req.user?.id })
+      .first();
+    if (!meal) {
+      return res.status(404).send({ error: "Meal not found" });
+    }
+    await knex("meals").where({ id, user_id: req.user?.id }).update({
+      name,
+      description,
+      is_on_diet: isOnDiet,
+      date: date.getTime(),
+    });
+
+    res.send();
+  });
+  app.get("/", { preHandler: [checkSessionIdExists] }, async (req, res) => {
+    const meals = await knex("meals")
+      .where({ user_id: req.user?.id })
+      .select("*");
+    
+    return res.status(200).send(meals);
+  });
 }
